a35c6fb86b68b28e68c33873fc207ee7aa1b970d
이어진 <zik@mz.co.kr>
2019-05-06 15:21:32 +0900

    123123

--- src/views/chatRoom/chatMode.vue
+++ src/views/chatRoom/chatMode.vue
@@ -3,7 +3,7 @@
     <div  style="width:100%;height:600px;overflow:scroll;" id="chatBox">
       <container v-for="item in questions" style="color: gray;width:80%;float:left;background-color: #f1f1f1;border: 2px solid #dedede;  border-radius: 5px;padding: 10px;margin: 10px 0;">{{item}}</container>
     </div>
-    <input type="text" style="position:absolute;bottom:0;left:0;width:80%;height:40px;" v-model="question">
+    <input type="text" @keyup.enter="submit" style="position:absolute;bottom:0;left:0;width:80%;height:40px;" v-model="question">
     <button @click="submit" style="width:20%;height:40px;float: right;color: white;background-color: lightsteelblue">submit</button>
   </div>
 </template>
@@ -21,7 +21,7 @@
         if(this.question.length>0){
           this.questions.push(this.question)
           this.question=''
-
+          
           $('#chatBox').animate({ scrollTop:10000000000000},300)
         }
 
